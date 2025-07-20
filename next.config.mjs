febd79b11c8c670cca41ef327daf8608b0390bd4
Bintang Pradana Erlangga Putra <bintang.pradana@privyid.tech>
2025-07-21 00:31:50 +0700

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;

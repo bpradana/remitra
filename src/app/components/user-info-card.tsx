d4d87f4b1dbddd226d13a266c0805129793e6987
Yustina Yasin <yustinayasin@Yustinas-MacBook-Pro.local>
2025-07-22 23:02:04 +0700

    add: edit profile and bank

--- src/app/components/user-info-card.tsx
+++ src/app/components/user-info-card.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ExternalLink, Copy } from "lucide-react";
+import { ExternalLink, Copy, Settings, CreditCard } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -19,6 +19,34 @@ import {
 import { formatAddress, greetUser } from "@/lib/utils";
 import { useUser, useSmartAccountClient } from "@account-kit/react";
 import { Spinner } from "@/components/ui/spinner";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
 
 export default function UserInfo() {
   const [isAddressCopied, setIsAddressCopied] = useState(false);
@@ -126,7 +154,9 @@ export default function UserInfo() {
           </p>
           <p className="font-medium">{userEmail}</p>
         </div>
-        {loading ? <Spinner /> : userInfo && !userInfo.userName ? (
+        {loading ? (
+          <Spinner />
+        ) : userInfo && !userInfo.userName ? (
           <div className="mb-4">
             <div className="flex items-center gap-2 mb-1">
               <p className="text-sm font-medium text-muted-foreground">
@@ -138,12 +168,15 @@ export default function UserInfo() {
                 Please create a username to continue.
               </p>
             </div>
-            <form onSubmit={handleUsernameSubmit} className="flex gap-2 items-center">
+            <form
+              onSubmit={handleUsernameSubmit}
+              className="flex gap-2 items-center"
+            >
               <div className="flex items-center gap-2">
                 <Input
                   placeholder="Enter username"
                   value={username}
-                  onChange={e => setUsername(e.target.value)}
+                  onChange={(e) => setUsername(e.target.value)}
                   disabled={isSubmitting}
                 />
                 <Button type="submit" size="sm" disabled={isSubmitting}>
@@ -230,6 +263,103 @@ export default function UserInfo() {
               <ExternalLink className="h-3.5 w-3.5" />
             </Button>
           </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <Dialog>
+            <form>
+              <DialogTrigger asChild>
+                <Button variant="outline">
+                  <Settings className="h-4 w-4" /> Edit Account
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Edit profile</DialogTitle>
+                  <DialogDescription>
+                    Make changes to your profile here. Click save when
+                    you&apos;re done.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4">
+                  <div className="grid gap-3">
+                    <Label htmlFor="name-1">Email</Label>
+                    <Input id="name-1" name="name" defaultValue={userEmail} />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="username-1">Username</Label>
+                    <Input
+                      id="username-1"
+                      name="username"
+                      defaultValue={userInfo?.userName ?? "@ystnysn"}
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="nik-1">NIK</Label>
+                    <Input id="nik-1" name="nik" placeholder="16 digits" />
+                  </div>
+                  <div className="grid w-full max-w-sm items-center gap-3">
+                    <Label htmlFor="ktp">KTP</Label>
+                    <Input id="ktp" type="file" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline">Cancel</Button>
+                  </DialogClose>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </form>
+          </Dialog>
+          <Dialog>
+            <form>
+              <DialogTrigger asChild>
+                <Button variant="outline">
+                  <CreditCard className="h-4 w-4" /> Bank
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Bank</DialogTitle>
+                  <DialogDescription>
+                    Make changes to your banks account here. Click save when
+                    you&apos;re done.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4">
+                  <div className="grid gap-3">
+                    <Label htmlFor="fullname-1">Full name</Label>
+                    <Input id="fullname-1" name="fullname" />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="bankname-1">Bank Name</Label>
+                    <Select>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a verified bank" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="bri">BRI</SelectItem>
+                        <SelectItem value="bca">BCA</SelectItem>
+                        <SelectItem value="mandiri">Mandiri</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="bank-count-number-1">
+                      Bank Account Number
+                    </Label>
+                    <Input id="bank-count-number-1" name="bank-count-number" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="outline">Cancel</Button>
+                  </DialogClose>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </form>
+          </Dialog>
         </div>
       </CardContent>
     </Card>
